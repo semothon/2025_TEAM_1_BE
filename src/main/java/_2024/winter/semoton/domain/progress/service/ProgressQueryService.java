a8ed1ef064811b6e29d55262d7f1772cd3317efa
junho <jh102328@khu.ac.kr>
2025-04-06 07:49:36 +0900

    .

--- src/main/java/_2024/winter/semoton/domain/progress/service/ProgressQueryService.java
+++ src/main/java/_2024/winter/semoton/domain/progress/service/ProgressQueryService.java
@@ -58,7 +58,7 @@ public class ProgressQueryService {
             landmarkList.add(
                     new LandmarkBriefDto(
                             landmark.getName(),
-                            isWithinDistance(latitude, longitude, landmark.getLatitude(), landmark.getLongitude(), 1000)
+                            isWithinDistance(latitude, longitude, landmark.getLatitude(), landmark.getLongitude(), 300)
                     )
             );
         }
